2f5bd7435118bfc9efec9aa921d843fa87c19774
Eason <uu9924079@gmail.com>
2024-10-10 16:18:59 +0800

    chore: add list

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,50 +5,55 @@
       <f7-nav-left>
         <f7-link icon-ios="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
       </f7-nav-left>
-      <f7-nav-title sliding>rent-client</f7-nav-title>
-      <f7-nav-right>
-        <f7-link icon-ios="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
-      </f7-nav-right>
-      <f7-nav-title-large>rent-client</f7-nav-title-large>
+      <f7-nav-title-large>租屋懶人包</f7-nav-title-large>
     </f7-navbar>
 
     <!-- Page content-->
-    <f7-block>
-      <p>This is an example of tabs-layout application. The main point of such tabbed layout is that each tab contains independent view with its own routing and navigation.</p>
-
-      <p>Each tab/view may have different layout, different navbar type (dynamic, fixed or static) or without navbar like this tab.</p>
-    </f7-block>
-    <f7-block-title>Navigation</f7-block-title>
-    <f7-list strong inset dividersIos>
-      <f7-list-item link="/about/" title="About"></f7-list-item>
-      <f7-list-item link="/form/" title="Form"></f7-list-item>
-    </f7-list>
-
-    <f7-block-title>Modals</f7-block-title>
-    <f7-block class="grid grid-cols-2 grid-gap">
-      <f7-button fill popup-open="#my-popup">Popup</f7-button>
-      <f7-button fill login-screen-open="#my-login-screen">Login Screen</f7-button>
+    <f7-block v-for="item in rentData" strong inset>
+      <f7-block-title>{{ item.title }}</f7-block-title>
+      <swiper-container :pagination="true" class="demo-swiper-multiple" :space-between="12">
+        <swiper-slide v-for="img in item.images">
+          <img :src="img" :alt="item.title" loading="lazy">
+        </swiper-slide>
+      </swiper-container>
+      <div :class="$style.info">
+        <span>{{ item.address }}</span>
+        <span>{{ item.area }}</span>
+        <f7-link :href="item.link" :external="true" target="_blank">連結</f7-link>
+      </div>
+      <p :class="$style.price">${{ item.price }}</p>
     </f7-block>
 
-    <f7-block-title>Panels</f7-block-title>
-    <f7-block class="grid grid-cols-2 grid-gap">
-      <f7-button fill panel-open="left">Left Panel</f7-button>
-      <f7-button fill panel-open="right">Right Panel</f7-button>
-    </f7-block>
-
-    <f7-list strong inset dividersIos>
-      <f7-list-item
-        title="Dynamic (Component) Route"
-        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
-      ></f7-list-item>
-      <f7-list-item
-        title="Default Route (404)"
-        link="/load-something-that-doesnt-exist/"
-      ></f7-list-item>
-      <f7-list-item
-        title="Request Data & Load"
-        link="/request-and-load/user/123456/"
-      ></f7-list-item>
-    </f7-list>
   </f7-page>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import axios from 'axios';
+interface I591NormalRes {
+  "title": string
+  "price": string
+  "address": string
+  "floor": string
+  "area": string
+  "images": string[]
+  "link": string
+}
+let rentData = ref<I591NormalRes[]>([])
+axios.get('http://localhost:5500/temp/591normal.json').then(res => {
+  rentData.value = res.data
+})
+</script>
+
+<style lang="css" module>
+.info {
+  display: flex;
+  gap: 12px;
+}
+.price {
+  text-align: right;
+  font-size: 24px;
+  color: red;
+  margin: 4px 0;
+}
+</style>
